35fe843c147a0a2a7a2d634aca51e7abde0c4105
pattessm <sam.patteson@moovel.com>
2018-07-21 18:25:52 -0500

    First push network framework

---
+++ ModeratorNetwork/ModeratorNetwork.h
@@ -0,0 +1,19 @@
+//
+//  ModeratorNetwork.h
+//  ModeratorNetwork
+//
+//  Created by Sam Patteson on 7/21/18.
+//  Copyright © 2018 Sam Patteson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ModeratorNetwork.
+FOUNDATION_EXPORT double ModeratorNetworkVersionNumber;
+
+//! Project version string for ModeratorNetwork.
+FOUNDATION_EXPORT const unsigned char ModeratorNetworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ModeratorNetwork/PublicHeader.h>
+
+
